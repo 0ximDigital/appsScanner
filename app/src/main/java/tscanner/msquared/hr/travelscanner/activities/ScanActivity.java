aa2d0443cf1f9b6e7a22e9afa439fc96d8b2641f
Mihael Francekovic <mihael.francekovic@fer.hr>
2015-05-23 13:22:50 +0200

    init commit

---
+++ app/src/main/java/tscanner/msquared/hr/travelscanner/activities/ScanActivity.java
@@ -0,0 +1,17 @@
+package tscanner.msquared.hr.travelscanner.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import tscanner.msquared.hr.travelscanner.R;
+
+
+public class ScanActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+    }
+
+}
