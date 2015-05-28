618c33e02223907e2786443f0cde312e322a9d29
Mihael Francekovic <mihael.francekovic@fer.hr>
2015-05-29 01:26:09 +0200

    rest classes

---
+++ app/src/main/java/tscanner/msquared/hr/travelscanner/models/TestModel.java
@@ -0,0 +1,7 @@
+package tscanner.msquared.hr.travelscanner.models;
+
+/**
+ * Created by Mihael on 29.5.2015..
+ */
+public class TestModel {
+}
---
+++ app/src/main/java/tscanner/msquared/hr/travelscanner/models/restModels/AppUser.java
@@ -0,0 +1,77 @@
+package tscanner.msquared.hr.travelscanner.models.restModels;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Mihael on 29.5.2015..
+ */
+public class AppUser {
+
+    @Expose
+    private String username;
+    @Expose
+    private Integer id;
+    @SerializedName("is_salesman")
+    @Expose
+    private Boolean isSalesman;
+    @SerializedName("travel_points")
+    @Expose
+    private Integer travelPoints;
+    @Expose
+    private String email;
+
+    public AppUser(String email, Integer id, Boolean isSalesman, Integer travelPoints, String username) {
+        this.email = email;
+        this.id = id;
+        this.isSalesman = isSalesman;
+        this.travelPoints = travelPoints;
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Boolean isSalesman() {
+        return isSalesman;
+    }
+
+    public void setIsSalesman(Boolean isSalesman) {
+        this.isSalesman = isSalesman;
+    }
+
+    public Integer getTravelPoints() {
+        return travelPoints;
+    }
+
+    public void setTravelPoints(Integer travelPoints) {
+        this.travelPoints = travelPoints;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id + ", " + username + ", " + email + ", isSalesman: " + isSalesman + ", travelPoints: " + travelPoints;
+    }
+
+}
