e8008e1762654c38221709deea2a38347e834de9
Mihael Francekovic <mihael.francekovic@fer.hr>
2015-05-29 18:41:36 +0200

    serverManager skeleton

--- app/src/main/java/tscanner/msquared/hr/travelscanner/helpers/ServerManager.java
+++ app/src/main/java/tscanner/msquared/hr/travelscanner/helpers/ServerManager.java
@@ -1,5 +1,16 @@
 package tscanner.msquared.hr.travelscanner.helpers;
 
+import java.util.List;
+
+import tscanner.msquared.hr.travelscanner.helpers.Rest.DeleteRestService;
+import tscanner.msquared.hr.travelscanner.helpers.Rest.GetRestService;
+import tscanner.msquared.hr.travelscanner.helpers.Rest.PostRestService;
+import tscanner.msquared.hr.travelscanner.helpers.Rest.PutRestService;
+import tscanner.msquared.hr.travelscanner.models.restModels.AppUser;
+import tscanner.msquared.hr.travelscanner.models.restModels.Purchase;
+import tscanner.msquared.hr.travelscanner.models.restModels.TravelDestination;
+import tscanner.msquared.hr.travelscanner.models.restModels.Traveler;
+
 /**
  * Created by Mihael on 29.5.2015..
  */
@@ -7,6 +18,59 @@ public class ServerManager {
 
     private final String TAG = this.getClass().getName();
 
+    private GetRestService getRestService;
+    private PostRestService postRestService;
+    private PutRestService putRestService;
+    private DeleteRestService deleteRestService;
+
     public ServerManager() {
+    }
+
+    public List<AppUser> getAllAppUsers(){
+        throw new UnsupportedOperationException("TODO - Implement this !");
+    }
+
+    public AppUser getAppUserById(Integer id){
+        throw new UnsupportedOperationException("TODO - Implement this !");
+    }
+
+    public List<AppUser> getAllSalesmanUsers(){
+        throw new UnsupportedOperationException("TODO - Implement this !");
+    }
+
+    public List<AppUser> getAllNonSalesmanUsers(){
+        throw new UnsupportedOperationException("TODO - Implement this !");
+    }
+
+    public List<TravelDestination> getAllTravelDestiantions(){
+        throw new UnsupportedOperationException("TODO - Implement this !");
+    }
+
+    public TravelDestination getTravelDestinationById(Integer id){
+        throw new UnsupportedOperationException("TODO - Implement this !");
+    }
+
+    public List<Purchase> getAllPurchases(){
+        throw new UnsupportedOperationException("TODO - Implement this !");
+    }
+
+    public Purchase getPurchaseById(Integer id){
+        throw new UnsupportedOperationException("TODO - Implement this !");
+    }
+
+    public List<Purchase> getAllPurchasesFromUserId(Integer id){
+        throw new UnsupportedOperationException("TODO - Implement this !");
+    }
+
+    public List<Traveler> getAllTravelers(){
+        throw new UnsupportedOperationException("TODO - Implement this !");
+    }
+
+    public Traveler getTravelerById(Integer id){
+        throw new UnsupportedOperationException("TODO - Implement this !");
+    }
+
+    public List<Traveler> getTravelersWithPurchaseId(Integer id){
+        throw new UnsupportedOperationException("TODO - Implement this !");
     }
 }
