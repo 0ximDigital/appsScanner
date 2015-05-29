fd3869306e3a95194a3ae7b5bd0861a2b613a95c
Mihael Francekovic <mihael.francekovic@fer.hr>
2015-05-29 18:07:22 +0200

    api constants

---
+++ app/src/main/java/tscanner/msquared/hr/travelscanner/helpers/ServerManager.java
@@ -0,0 +1,12 @@
+package tscanner.msquared.hr.travelscanner.helpers;
+
+/**
+ * Created by Mihael on 29.5.2015..
+ */
+public class ServerManager {
+
+    private final String TAG = this.getClass().getName();
+
+    public ServerManager() {
+    }
+}
