dbde7fa7060d9598722cd66e24f67c5e68f66b5b
Mihael Francekovic <mihael.francekovic@fer.hr>
2015-05-31 16:20:31 +0200

    jednostavno sam predobar s ovim animacijama

--- app/src/main/java/tscanner/msquared/hr/travelscanner/activities/AddDestinationTravelersActivity.java
+++ app/src/main/java/tscanner/msquared/hr/travelscanner/activities/AddDestinationTravelersActivity.java
@@ -1,20 +1,31 @@
 package tscanner.msquared.hr.travelscanner.activities;
 
+import android.animation.Animator;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.github.clans.fab.FloatingActionButton;
 import com.google.gson.Gson;
 import com.kogitune.activity_transition.ActivityTransition;
+import com.kogitune.activity_transition.ActivityTransitionLauncher;
+import com.kogitune.activity_transition.ExitActivityTransition;
 import com.squareup.picasso.Picasso;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import tscanner.msquared.hr.travelscanner.R;
+import tscanner.msquared.hr.travelscanner.customViews.CustomDestinationInfoView;
 import tscanner.msquared.hr.travelscanner.customViews.TravelerView;
 import tscanner.msquared.hr.travelscanner.models.restModels.TravelDestination;
 
@@ -35,30 +46,44 @@ public class AddDestinationTravelersActivity extends Activity {
     private ImageView destinationBottomImage;
     private TextView firstTravelerText;
 
+    // this needs to be hidden first - GONE
     private FloatingActionButton addTravelerButton;
+    private FrameLayout contentFrameLayout;
 
     private Gson gson;
 
     private TravelDestination travelDestination;
+
+    private ActivityTransition entryAnimation;
+    private ExitActivityTransition exitActivityTransition;
+
+    private Timer timer;
+    private TimerTask timerTask;
+    private final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_destination_travelers);
 
-        this.destinationBottomImage = (ImageView) findViewById(R.id.imgDestinationBottom);
-        String imgUrl = getIntent().getStringExtra(this.DESTINATION_IMAGE_URL_EXTRA);
-        Picasso.with(this).load(imgUrl).into(this.destinationBottomImage);
-        ActivityTransition.with(getIntent()).to(findViewById(R.id.imgDestinationBottom)).duration(SharedAnimationDurationMillis).start(savedInstanceState);
-
         this.gson = new Gson();
         this.referenceViews();
+
+        this.hideAllViewsForTransition(null);
+
+        String imgUrl = getIntent().getStringExtra(this.DESTINATION_IMAGE_URL_EXTRA);
+        Picasso.with(this).load(imgUrl).into(this.destinationBottomImage);
+        this.entryAnimation = ActivityTransition.with(getIntent()).to(this.destinationBottomImage).duration(SharedAnimationDurationMillis);
+        this.exitActivityTransition = this.entryAnimation.start(savedInstanceState);
+        this.startTimer();
     }
 
     private void referenceViews(){
 
         this.containerLinearLayout = (LinearLayout) findViewById(R.id.containerLinearLayout);
+
         this.addTravelerButton = (FloatingActionButton) findViewById(R.id.btnAddDestinationTraveler);
+        this.contentFrameLayout = (FrameLayout) findViewById(R.id.contentFrameLayout);
 
         this.destinationBottomImage = (ImageView) findViewById(R.id.imgDestinationBottom);
         String destinationData = getIntent().getStringExtra(this.DESTINATION_SERIALIZED_DATA);
@@ -78,6 +103,79 @@ public class AddDestinationTravelersActivity extends Activity {
                 travelerView.setTravelerTitleName("Traveler -> " + new Random().nextInt());
                 containerLinearLayout.addView(travelerView);
                 firstTravelerText.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private interface ViewsHideListener{
+        void onCompletion();
+    }
+
+    private void hideAllViewsForTransition(final ViewsHideListener viewsHideListener){
+        if(viewsHideListener == null){
+            this.contentFrameLayout.animate().alpha(0.0f).setDuration(0).start();
+        }
+        else{
+            this.contentFrameLayout.animate().alpha(0.0f).setDuration(SharedAnimationDurationMillis/2).setListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                    if(viewsHideListener != null){
+                        viewsHideListener.onCompletion();
+                    }
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+
+                }
+            });
+        }
+    }
+
+    private void showAllViewsAfterTransition(){
+        this.contentFrameLayout.animate().alpha(1.0f).setDuration(SharedAnimationDurationMillis/2).start();
+    }
+
+    private void startTimer(){
+        this.timer = new Timer();
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        showAllViewsAfterTransition();
+                    }
+                }, SharedAnimationDurationMillis);
+                stopTimer();
+            }
+        };
+        this.timer.schedule(this.timerTask, 0, this.SharedAnimationDurationMillis + 100);
+    }
+
+    private void stopTimer(){
+        if(this.timerTask != null){
+            this.timer.cancel();
+            this.timer = null;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        this.hideAllViewsForTransition(new ViewsHideListener() {
+            @Override
+            public void onCompletion() {
+                exitActivityTransition.exit(AddDestinationTravelersActivity.this);
             }
         });
     }
