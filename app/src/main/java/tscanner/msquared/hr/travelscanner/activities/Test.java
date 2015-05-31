88f796aac7e3ecc5f286e19f23cedb31d2720fac
Mihael Francekovic <mihael.francekovic@fer.hr>
2015-05-31 14:06:19 +0200

    micanje travlera

--- app/src/main/java/tscanner/msquared/hr/travelscanner/activities/Test.java
+++ app/src/main/java/tscanner/msquared/hr/travelscanner/activities/Test.java
@@ -3,6 +3,7 @@ package tscanner.msquared.hr.travelscanner.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import tscanner.msquared.hr.travelscanner.customViews.CustomMaterialDialog;
 import tscanner.msquared.hr.travelscanner.R;
@@ -13,7 +14,7 @@ import tscanner.msquared.hr.travelscanner.customViews.TravelerView;
  */
 public class Test extends Activity {
 
-    private CustomMaterialDialog cmd;
+    private LinearLayout containerLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,15 @@ public class Test extends Activity {
     }
 
     private void referenceViews(){
+
+        this.containerLinearLayout = (LinearLayout) findViewById(R.id.containerLinearLayout);
+
+        for(int i=0; i<5; i++){
+            TravelerView view = new TravelerView(this, this.containerLinearLayout);
+            view.setTravelerTitleName("Traveler -> " + i);
+            this.containerLinearLayout.addView(view);
+        }
+
         /*
         TravelerView tv=new TravelerView(this);
         this.cmd.addTravelerView(tv);
--- app/src/main/java/tscanner/msquared/hr/travelscanner/customViews/TravelerView.java
+++ app/src/main/java/tscanner/msquared/hr/travelscanner/customViews/TravelerView.java
@@ -3,8 +3,10 @@ package tscanner.msquared.hr.travelscanner.customViews;
 import android.animation.Animator;
 import android.animation.Animator.AnimatorListener;
 import android.content.Context;
+import android.media.Image;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -20,14 +22,20 @@ public class TravelerView extends FrameLayout {
     private final String TAG = getClass().getSimpleName();
     private Context context;
 
+    private ViewGroup parent;
+
     private FrameLayout travelersContainer;
     private LinearLayout travelersInfo;
-    private TextView travelerName;
-    private ImageView addTravelerImage;
-    private ImageView deleteTravelerImage;
 
-    public TravelerView(Context context) {
+    private TextView travelerTitleName;
+    private TextView travelerNameAndSurname;
+    private TextView travelerBirthDate;
+    private TextView travelerIdNumber;
+    private ImageView deleteTravelerIcon;
+
+    public TravelerView(Context context, ViewGroup parent) {
         super(context);
+        this.parent = parent;
         this.init(context);
     }
 
@@ -50,12 +58,28 @@ public class TravelerView extends FrameLayout {
     private void reference() {
         this.travelersInfo = (LinearLayout) findViewById(R.id.TravelerViewTravelersInfoLinearLayout);
         this.travelersContainer = (FrameLayout) findViewById(R.id.TravelerViewTravelersContainerFrameLayout);
+
+        this.travelerTitleName = (TextView) findViewById(R.id.txtTravelerNameTitle);
+        this.travelerNameAndSurname = (TextView) findViewById(R.id.txtTravelerNameAndSurname);
+        this.travelerBirthDate = (TextView) findViewById(R.id.txtTravelerBirthDate);
+        this.travelerIdNumber = (TextView) findViewById(R.id.txtTravelerIdNumber);
+
+        this.deleteTravelerIcon = (ImageView) findViewById(R.id.imgBtnRemoveTraveler);
+        this.deleteTravelerIcon.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (parent != null) {
+                    parent.removeView(TravelerView.this);
+                }
+            }
+        });
+
         this.travelersContainer.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(travelersInfo.getVisibility() == VISIBLE){
+                if (travelersInfo.getVisibility() == VISIBLE) {
                     travelersInfo.animate().translationX(travelersInfo.getWidth() * -1).setDuration(400).setListener(travelerInfoAnimatorListener);
-                } else{
+                } else {
                     travelersInfo.setVisibility(VISIBLE);
                     travelersInfo.animate().translationX(0).setDuration(399).setListener(travelerInfoAnimatorListener);
                 }
@@ -63,8 +87,20 @@ public class TravelerView extends FrameLayout {
         });
     }
 
-    public void setTravelerName(String travelerName){
-        this.travelerName.setText(travelerName);
+    public void setTravelerTitleName(String travelerTitleName){
+        this.travelerTitleName.setText(travelerTitleName);
+    }
+
+    public void setTravelerNameAndSurname(String travelerName){
+        this.travelerNameAndSurname.setText(travelerName);
+    }
+
+    public void setTravelerBirthDate(String travelerBirthDate){
+        this.travelerBirthDate.setText(travelerBirthDate);
+    }
+
+    public void setTravelerIdNumber(String travelerIdNumber){
+        this.travelerIdNumber.setText(travelerIdNumber);
     }
 
     private Animator.AnimatorListener travelerInfoAnimatorListener = new AnimatorListener() {
