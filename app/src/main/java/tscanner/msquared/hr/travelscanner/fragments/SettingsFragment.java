a0f5071f3de30b1697b86e2c22382fb32017cd35
Mihael Francekovic <mihael.francekovic@fer.hr>
2015-06-01 09:24:11 +0200

    working user data update

--- app/src/main/java/tscanner/msquared/hr/travelscanner/fragments/SettingsFragment.java
+++ app/src/main/java/tscanner/msquared/hr/travelscanner/fragments/SettingsFragment.java
@@ -1,5 +1,6 @@
 package tscanner.msquared.hr.travelscanner.fragments;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -12,25 +13,53 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.github.clans.fab.FloatingActionButton;
+import com.google.gson.Gson;
+
+import net.steamcrafted.loadtoast.LoadToast;
+
+import java.security.PrivateKey;
+
 import tscanner.msquared.hr.travelscanner.InputFieldsCheck;
 import tscanner.msquared.hr.travelscanner.R;
 import tscanner.msquared.hr.travelscanner.activities.RegistrationActivity;
 import tscanner.msquared.hr.travelscanner.activities.ScanActivity;
 import tscanner.msquared.hr.travelscanner.activities.SettingsActivity;
+import tscanner.msquared.hr.travelscanner.helpers.PrefsHelper;
+import tscanner.msquared.hr.travelscanner.helpers.Rest.ServerManager;
+import tscanner.msquared.hr.travelscanner.models.restModels.AppUser;
+import tscanner.msquared.hr.travelscanner.models.restModels.ResponseMessage;
 
 /**
  * Created by Matej on 5/31/2015.
  */
 public class SettingsFragment extends android.support.v4.app.Fragment {
-    String TAG=getClass().getName();
+    String TAG=getClass().getSimpleName();
 
     private EditText mUsernameView;
     private EditText mPasswordRepView;
     private EditText mPasswordView;
     private EditText mEmailView;
-    private Button btsSave;
+    private FloatingActionButton btsSave;
 
     private InputFieldsCheck ifc=new InputFieldsCheck();
+
+    private PrefsHelper prefsHelper;
+    private Gson gson;
+    private Context context;
+
+    private AppUser appUser;
+    private ServerManager serverManager;
+
+    private LoadToast loadToast;
+
+    String email;
+    String password;
+    String passwordR;
+    String username;
+
+    boolean[] cancel = {false};
+    View[] focusView = {null};
 
     public static final String TITLE = "Setting";
 
@@ -45,12 +74,20 @@ public class SettingsFragment extends android.support.v4.app.Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        final View v=inflater.inflate(R.layout.fragment_settings,container,false);
+        final View v=inflater.inflate(R.layout.fragment_settings, container, false);
         this.mUsernameView = (EditText) v.findViewById(R.id.usernameS);
         this.mPasswordView = (EditText) v.findViewById(R.id.passwordS);
         this.mPasswordRepView = (EditText)v.findViewById(R.id.passwordRepeatS);
         this.mEmailView = (AutoCompleteTextView) v.findViewById(R.id.emailS);
-        this.btsSave = (Button) v.findViewById(R.id.save);
+        this.btsSave = (FloatingActionButton) v.findViewById(R.id.saveFab);
+        this.context = getActivity();
+        this.prefsHelper = new PrefsHelper(context);
+        this.gson = new Gson();
+
+        this.loadToast = new LoadToast(getActivity());
+        this.loadToast.setTranslationY(300);
+
+        populateUserData();
 
         this.btsSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,12 +95,23 @@ public class SettingsFragment extends android.support.v4.app.Fragment {
                 attemptSave();
             }
         });
-
         return v;
+    }
+
+    private void populateUserData(){
+        String appUserData = this.prefsHelper.getString(PrefsHelper.LOGGED_IN_USER_APPUSER_DATA, null);
+        if(appUserData != null){
+            this.appUser = gson.fromJson(appUserData, AppUser.class);
+            if(this.appUser != null){
+                this.mEmailView.setText(this.appUser.getEmail());
+                this.mUsernameView.setText(this.appUser.getUsername());
+            }
+        }
     }
 
     public void attemptSave() {
 
+        this.loadToast.setText("Updating..").show();
         // Reset errors.
         mEmailView.setError(null);
         mPasswordView.setError(null);
@@ -71,13 +119,13 @@ public class SettingsFragment extends android.support.v4.app.Fragment {
         mUsernameView.setError(null);
 
         // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
-        String passwordR = mPasswordRepView.getText().toString();
-        String username = mUsernameView.getText().toString();
+        email = mEmailView.getText().toString();
+        password = mPasswordView.getText().toString();
+        passwordR = mPasswordRepView.getText().toString();
+        username = mUsernameView.getText().toString();
 
-        final boolean[] cancel = {false};
-        final View[] focusView = {null};
+        cancel = new boolean[]{false};
+        focusView = new View[]{null};
 
         if(!ifc.isPasswordValid(passwordR)){
             mPasswordRepView.setError((CharSequence) ifc.getErrorMessage());
@@ -89,7 +137,6 @@ public class SettingsFragment extends android.support.v4.app.Fragment {
             cancel[0] = true;
         }
 
-
         if(!ifc.isPasswordValid(password)) {
             mPasswordView.setError((CharSequence) ifc.getErrorMessage());
             focusView[0] = mPasswordView;
@@ -100,17 +147,19 @@ public class SettingsFragment extends android.support.v4.app.Fragment {
         ifc.validadeEmail(email, new InputFieldsCheck.FieldCheckCallback() {
             @Override
             public void checkedField(boolean isItValid) {
-                if(isItValid){
+                if (isItValid) {
                     //sve je oke
-                }
-                else{
+                } else {
                     mEmailView.setError((CharSequence) ifc.getErrorMessage());
                     focusView[0] = mEmailView;
                     cancel[0] = true;
                 }
+                proceedToFinalValidation();
             }
         });
+    }
 
+    private void proccedToUsernameValidation(){
         ifc.validateUsername(username, new InputFieldsCheck.FieldCheckCallback() {
             @Override
             public void checkedField(boolean isItValid) {
@@ -122,22 +171,41 @@ public class SettingsFragment extends android.support.v4.app.Fragment {
                     focusView[0] = mUsernameView;
                     cancel[0] = true;
                 }
+                proceedToFinalValidation();
             }
         });
+    }
 
-
+    private void proceedToFinalValidation(){
         if (cancel[0]) {
+            this.loadToast.error();
             Log.d(TAG, "invalid change settings");
             focusView[0].requestFocus();
         } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            //TODO change data in base
-            startActivity(new Intent(getActivity(), SettingsActivity.class));
+            appUser.setEmail(email);
+            appUser.setPassword(password);
+            appUser.setUsername(username);
+            updateUser(appUser);
         }
     }
 
-
+    private void updateUser(AppUser user){
+        if(serverManager == null){
+            serverManager = new ServerManager();
+        }
+        serverManager.updateAppUserWithId(user.getId(), user, new ServerManager.Callback<ResponseMessage>() {
+            @Override
+            public void requestResult(ResponseMessage responseMessage) {
+                if (responseMessage.getError() == null) {
+                    loadToast.success();
+                    startActivity(new Intent(context, SettingsActivity.class));
+                } else {
+                    Log.e(TAG, responseMessage.getError());
+                    loadToast.error();
+                }
+            }
+        });
+    }
 
 
  }
